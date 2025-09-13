7ea6062c4bda6a4b8bf687698cf31278c2dd27e3
Garryrkk <garimakalra561@gmail.com>
2025-09-14 00:33:56 +0530

    deleted faalltu files

--- OneDrive/Documents/vs/gen jacx/Interviewer-AI/agenda frontend/electronService.js
+++
@@ -1,6 +0,0 @@
-// src/services/electronService.js
-export function sendHiddenAnswerToOverlay(answer) {
-  if (window?.electronAPI?.sendHiddenAnswer) {
-    window.electronAPI.sendHiddenAnswer({ answer, time: Date.now() });
-  }
-}
